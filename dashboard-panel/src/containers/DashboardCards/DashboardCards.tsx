3990a1fd3214e6fd816f68e9ceeb81485b413756
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2026-02-16 13:01:40 +0530

    fixed style issue

--- dashboard-panel/src/containers/DashboardCards/DashboardCards.tsx
+++ dashboard-panel/src/containers/DashboardCards/DashboardCards.tsx
@@ -9,25 +9,25 @@ const DashboardCards = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
-      <div className="bg-blue-500 text-white rounded-xl shadow-lg p-8 flex justify-between items-center">
+      <div className="bg-blue-500 text-white rounded-2xl shadow-lg p-8 flex justify-between items-center">
         <div>
-          <h2 className="text-3xl font-bold">{users?.length ?? 0}</h2>
+          <h2 className="text-3xl font-bold mb-3">{users?.length ?? 0}</h2>
           <p className="text-sm mt-1">Total Users</p>
         </div>
         <Users className="size-9" />
       </div>
 
-      <div className="bg-green-500 text-white rounded-2xl shadow-lg p-6 flex justify-between items-center">
+      <div className="bg-green-500 text-white rounded-2xl shadow-lg p-8 flex justify-between items-center">
         <div>
-          <h2 className="text-3xl font-bold">{products?.length ?? 0}</h2>
+          <h2 className="text-3xl font-bold mb-3">{products?.length ?? 0}</h2>
           <p className="text-sm mt-1">Total Products</p>
         </div>
         <Package className="size-9" />
       </div>
 
-      <div className="bg-orange-500 text-white rounded-2xl shadow-lg p-6 flex justify-between items-center">
+      <div className="bg-orange-500 text-white rounded-2xl shadow-lg p-8 flex justify-between items-center">
         <div>
-          <h2 className="text-3xl font-bold">{carts?.length ?? 0}</h2>
+          <h2 className="text-3xl font-bold mb-3">{carts?.length ?? 0}</h2>
           <p className="text-sm mt-1">Total Carts</p>
         </div>
         <ShoppingCart className="size-9" />

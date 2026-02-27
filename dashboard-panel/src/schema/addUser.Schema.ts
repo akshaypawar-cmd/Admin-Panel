79441d9714bc636eae13343a35dfe5d50ffdcd66
akshaypawar-cmd <akshay.pawar@miraiedge.com>
2026-02-27 11:49:53 +0530

    add shchema user section

---
+++ dashboard-panel/src/schema/addUser.Schema.ts
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+export const addUserSchema = yup.object({
+  firstname: yup
+    .string()
+    .required("FirstName is required")
+    .min(3, "minimum characters out of 3"),
+
+  lastname: yup
+    .string()
+    .required("LastName is required")
+    .min(3, "minimum characters out of 3"),
+
+  phone: yup
+    .number()
+    .required("Number is required")
+    .positive("number must be less than 0"),
+
+  address: yup
+  .string()
+  .required("address is required"),
+});
